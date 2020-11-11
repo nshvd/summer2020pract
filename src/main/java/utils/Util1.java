7f52ac2e7473e99347b81f732676924d065098a8
Jiydegul Asanova <jiydegulasanova@Jiydeguls-MacBook-Air.local>
2020-11-10 21:04:48 -0600

    hi

--- src/main/java/utils/Util1.java
+++ src/main/java/utils/Util1.java
@@ -4,4 +4,7 @@ public class Util1 {
     public static void login(String username, String password) {
         System.out.println("logging in using: " + username + ", pswd:" + password);
     }
+    public static void searchFor(String searchValue){
+        System.out.println("Searching for etc.");
+    }
 }
