2e70ee0f61af6bd02ebcb6415e49a464c646946b
elvinabegimbaeva <begelvina@gmail.com>
2020-11-10 21:58:24 -0500

    Subtring

---
+++ src/main/java/utils/Substring.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class Substring {
+    public static void main(String[] args) {
+
+        String s ="WelcomeToDevxSchool";
+        String s1 = s.substring(9,13);
+        String s2 = s.substring(13, s.length());
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+
+}
