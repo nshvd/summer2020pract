8e644fd900b7236c9a9e802b8e89caabdb7956c2
omonaza <aza.osmonbekov93@gmail.com>
2020-11-18 06:55:35 +0400

    conflict

--- src/main/java/utils/NazarsUtil.java
+++ src/main/java/utils/NazarsUtil.java
@@ -25,4 +25,8 @@ public class NazarsUtil {
     public static void sayHello2(String name, String lastName) {
         System.out.println("Hello: " + name + lastName);
     }
+
+    public static void conflictMakingFromAzamat(){
+        System.out.println("is conflict made ?");
+    }
 }
