10ef5e803c50d789663caa631e82d17b21270b36
elvinabegimbaeva <begelvina@gmail.com>
2020-11-17 22:01:35 -0500

    Add sum method

--- src/main/java/utils/NazarsUtil.java
+++ src/main/java/utils/NazarsUtil.java
@@ -29,4 +29,7 @@ public class NazarsUtil {
     public static void weather(String day, String night){
         System.out.println("Today's weather is" + day + night);
     }
+    public static void printSum(int i, int j){
+        System.out.println(i +j);
+    }
 }
