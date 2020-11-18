ac4291f98585b1236f15b10b45ccb4974de55124
Mirgul-Abdralieva <beniceontime@gmail.com>
2020-11-18 07:23:36 +0600

    This is update

--- src/main/java/utils/MirgulsUtil.java
+++ src/main/java/utils/MirgulsUtil.java
@@ -1,2 +1,11 @@
-package utils;public class MirgulsUtil {
+package utils;
+
+public class MirgulsUtil {
+
+    public static void Everest(){
+        System.out.println("I like Java!");
+    }
+    public static void  sayBye(){
+        System.out.println("Bye Bye!:::");
+    }
 }
--- src/main/java/utils/Util1.java
+++ src/main/java/utils/Util1.java
@@ -5,6 +5,7 @@ public class Util1 {
     public static void login(String username, String password) {
         System.out.println("logging in using: " + username + ", pswd:" + password);
     }
+
     public static void search(){
         System.out.println("Hi Hello!");
     }
