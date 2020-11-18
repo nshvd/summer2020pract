ba7cb8d0d470bc042e002455ca7d8079b209da4e
iRahatsevich <irene.rahatsevich@gmail.com>
2020-11-17 19:20:57 -0600

    greetings method

--- src/main/java/utils/IrynaCl.java
+++ src/main/java/utils/IrynaCl.java
@@ -3,6 +3,6 @@ package utils;
 public class IrynaCl {
 
     public static void heyHello(){
-        System.out.println("Greetings form iryna's classgid");
+        System.out.println("Greetings form iryna's class");
     }
 }
