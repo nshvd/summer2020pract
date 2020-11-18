bca88e55b677f925ea2ce468f2054d4db5eb6251
elvinabegimbaeva <begelvina@gmail.com>
2020-11-17 20:18:09 -0500

    this is 2nd changes

--- src/main/java/utils/Substring.java
+++ src/main/java/utils/Substring.java
@@ -5,9 +5,14 @@ public class Substring {
 
         String s ="WelcomeToDevxSchool";
         String s1 = s.substring(9,13);
-        String s2 = s.substring(13, s.length());
         System.out.println(s1);
-        System.out.println(s2);
+
+        hello(s);
+
+    }
+    public static void hello(String name){
+        System.out.println("Hello" + name);
+
     }
 
 }
