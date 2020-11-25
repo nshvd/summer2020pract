86291dee0b9da8f847b0733dbdb1c37cecf81e01
Mirgul-Abdralieva <beniceontime@gmail.com>
2020-11-25 23:21:47 +0600

    Mirgul2

--- src/main/java/Jiyde.java
+++ src/main/java/Jiyde.java
@@ -4,9 +4,9 @@ public class Jiyde {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int nums = sc.nextInt();
-        for (int i=1; i<=nums; i++) {
-            for (int j=1; j<=nums; j++) {
+        int var = sc.nextInt();
+        for (int i=1; i<=var; i++) {
+            for (int j=1; j<=var; j++) {
 
                 System.out.printf ("%4d",i*j);
             }
