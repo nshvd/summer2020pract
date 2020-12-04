0b38f15cde5557154f1994a5e3e4552e1acd71f7
Jiydegul Asanova <jiydegulasanova@Jiydeguls-Air.attlocal.net>
2020-12-04 11:18:48 -0600

    new message

--- src/main/java/Jiyde.java
+++ src/main/java/Jiyde.java
@@ -2,13 +2,17 @@ import java.util.Scanner;
 
 public class Jiyde {
 
+    public Jiyde(){
+
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int nums = sc.nextInt();
         for (int i=1; i<=nums; i++) {
             for (int j=1; j<=nums; j++) {
 
-                System.out.printf ("%5d",i*j);
+                System.out.printf ("nums");
             }
             System.out.println();
         }
